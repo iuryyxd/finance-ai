bf2486b3c2bec6659e8230cc3f06fe8506ebdb7c
iury <iurysena74@gmail.com>
2024-11-27 20:34:44 -0300

    fix: home params

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -10,7 +10,7 @@ import ExpensesPerCategory from "./_components/expenses-per-category";
 import LastTransactions from "./_components/last-transactions";
 
 interface HomeProps {
-  searchParams: { month: string };
+  searchParams: { month?: string };
 }
 
 const Home = async ({ searchParams: { month } }: HomeProps) => {
