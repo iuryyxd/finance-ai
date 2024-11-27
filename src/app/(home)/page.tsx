f5469998d3139c73e99fea43a9e5531a31028d61
iury <iurysena74@gmail.com>
2024-11-27 20:06:50 -0300

    feat: add expenses per category component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,6 +6,7 @@ import TimeSelect from "./_components/time-select";
 import { isMatch } from "date-fns";
 import TransactionsPieChart from "./_components/transactions-pie-chart";
 import { getDashboard } from "@/data/get-dashboard";
+import ExpensesPerCategory from "./_components/expenses-per-category";
 
 interface HomeProps {
   searchParams: { month: string };
@@ -32,8 +33,11 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
         <div className="grid grid-cols-[2fr,1fr]">
           <div className="flex flex-col gap-6">
             <SummaryCards {...dashboard} />
-            <div className="grid grid-cols-3 grid-rows-1 gap-6">
+            <div className="grid h-full grid-cols-3 grid-rows-1 gap-6 overflow-hidden">
               <TransactionsPieChart {...dashboard} />
+              <ExpensesPerCategory
+                expensesPerCategory={dashboard.totalExpensePerCategory}
+              />
             </div>
           </div>
         </div>
